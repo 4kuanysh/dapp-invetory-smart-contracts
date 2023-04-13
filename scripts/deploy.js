577cb7c5b56bce8ff5818abfa3df29477cf01b10
Kuanysh <kuanysh@Kuanyss-MacBook-Pro-2.local>
2023-04-13 21:29:35 +0600

    add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,38 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [owner] = await ethers.getSigners();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const noteFactory = await ethers.getContractFactory("NFT");
+  const contract = await noteFactory.deploy();
+  await contract.deployed();
+  console.log(`Contract address ${contract.address}`);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(network.config);
+  if (network.config.chainId === 97) {
+    contract.deployTransaction.wait(6);
+    verify(contract.address, []);
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function verify(contractAddress, arguments) {
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: arguments,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase.includes("already verified")) {
+      console.log("The contract already verified");
+    } else {
+      console.log(e);
+    }
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
